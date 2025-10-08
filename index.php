1227b5ba22f3dd291462a18edc8f450cd3ff94a4
AdrienMertens <mertensadrien23@hotmail.be>
2025-10-08 19:25:14 +0200

    Add basic structure with header, footer, and index page

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require_once __DIR__ . DIRECTORY_SEPARATOR .'struct'.DIRECTORY_SEPARATOR. 'header.php'; ?>
+<h1>Accueil</h1>
+<?php require_once __DIR__ . DIRECTORY_SEPARATOR .'struct'.DIRECTORY_SEPARATOR. 'footer.php'; ?>
